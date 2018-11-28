19d59a592b2d8f965e65434451018bdab5f58972
VaishnaviThamshetty <39074336+VaishnaviThamshetty@users.noreply.github.com>
2018-11-28 22:04:31 +0530

    Delete division.js

--- division.js
+++
@@ -1,8 +0,0 @@
-function Div(arr){
-    var number1= Number(arr[0]);
-    var number2= Number(arr[1]);
-    result = number1/number2;
-    console.log('Division of',number1,'and',number2,'is',result) ;
-    
-  }
- Div(process.argv.slice(2));
