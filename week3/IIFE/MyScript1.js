880ee66a9ecdae829c03e963124c7bd5a3d7c292
VaishnaviThamshetty <39074336+VaishnaviThamshetty@users.noreply.github.com>
2018-11-28 21:53:07 +0530

    Add files via upload

---
+++ week3/IIFE/MyScript1.js
@@ -0,0 +1,10 @@
+(function () {
+    var userName = "Vaishu";
+    
+    function display(name)
+    {
+        console.log("MyScript1.js: " + name);
+    }
+
+    display(userName);
+  })();
---
+++ week3/IIFE/MyScript2.js
@@ -0,0 +1,19 @@
+/*(function () {
+    var userName = "Kuttu";
+    
+    function display(name)
+    {
+        console.log("MyScript2.js: " + name);
+    }
+
+    display(userName);
+  })();*/
+
+  var userName = "Steve";
+
+function display(name)
+{
+    console.log("MyScript2.js: " + name);
+}
+
+display(userName);
