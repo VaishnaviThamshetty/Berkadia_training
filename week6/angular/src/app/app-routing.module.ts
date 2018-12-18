ea381297348a2d473c048bf2282812d2f7a2ae3c
VaishnaviThamshetty <39074336+VaishnaviThamshetty@users.noreply.github.com>
2018-12-18 17:03:52 +0530

    Update app-routing.module.ts

--- week6/angular/src/app/app-routing.module.ts
+++ week6/angular/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListEmployeeComponent } from './employee/list-employee/list-employee.component';
 import { RatingEmployeeComponent } from './employee/rating-employee/rating-employee.component';
-
+import { PosEmployeeComponent } from './employee/pos-employee/pos-employee.component';
 const routes: Routes = [
   {path: 'list' ,component: ListEmployeeComponent},
   {path: 'rating' ,component: RatingEmployeeComponent},
+  {path: 'position' ,component: PosEmployeeComponent},
   {path: '' ,redirectTo:'/list', pathMatch:'full'} 
 ];
 
